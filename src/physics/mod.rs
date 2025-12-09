6460ca66212b9ea65887acef53105e9a31e5929f
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-12-09 13:54:30 +1100

    make colliding_with a free fn

--- src/physics/mod.rs
+++ src/physics/mod.rs
@@ -1,9 +1,6 @@
 use glam::Vec3;
 
 pub trait PhysicsBody {
-    /// return whether a body is colliding with another body
-    fn colliding_with(&self, other: &dyn PhysicsBody) -> bool;
-
     /// return the current position of the body
     fn position(&self) -> Vec3;
 
@@ -12,4 +9,20 @@ pub trait PhysicsBody {
 
     /// move the body by the delta provided
     fn translate(&mut self, delta: Vec3);
+}
+
+/// Check whether two physics bodies are colliding using AABB collision detection
+pub fn colliding_with<A: PhysicsBody, B: PhysicsBody>(a: &A, b: &B) -> bool {
+    let a_pos = a.position();
+    let a_size = a.size();
+    let b_pos = b.position();
+    let b_size = b.size();
+
+    // AABB collision detection (symmetric check)
+    a_pos.x < b_pos.x + b_size.x
+        && a_pos.x + a_size.x > b_pos.x
+        && a_pos.y < b_pos.y + b_size.y
+        && a_pos.y + a_size.y > b_pos.y
+        && a_pos.z < b_pos.z + b_size.z
+        && a_pos.z + a_size.z > b_pos.z
 }
